217154b57f35a037482c45e89286097a258aff2b
Michael Duncan <mwduncan2018@gmail.com>
2024-09-03 10:42:11 -0400

    Add view models

---
+++ ViewModels/FuzzyViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DuncanSafeApp2024.ViewModels
+{
+    public class FuzzyViewModel
+    {
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public double Price { get; set; }
+        [Display(Name = "Number In Stock")]
+        public int NumberInStock { get; set; }
+        [Display(Name = "Match?")]
+        public bool IsOnWatchList { get; set; }
+        [Display(Name = "Fuzzy Match?")]
+        public bool IsFuzzyMatch { get; set; }
+    }
+}
