6955a559a803ca3dc1dabae67f98c845ebbe215b
Michael Duncan <mwduncan2018@gmail.com>
2024-09-03 10:47:14 -0400

    Add css and JS

---
+++ wwwroot/js/contact.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    setTimeout(function () {
+
+        var secret = document.createElement("span");
+        var message = document.createTextNode(" (Duncan Safe Product!)");
+        secret.appendChild(message);
+        secret.id = "secretMessage";
+
+        var copyright = document.querySelector("#copyright");
+        copyright.appendChild(secret);
+
+    }, 2000);
+
+});
